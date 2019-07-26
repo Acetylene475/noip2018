9da9ca642261b0956f4f127ee2b786c8759049ab
Acetylene <52035894+Acetylene475@users.noreply.github.com>
2019-07-26 21:59:11 +0800

    commit

--- 普及组/1.标题统计/title.cpp
+++ 普及组/1.标题统计/title.cpp
@@ -1,16 +1,16 @@
 #include <cstdio>
-#include <string>
+#include <cstring>
 using namespace std;
 
 int main()
 {
     FILE *in,*out;
-    string s;
+    char s[127];
     int count=0;
     in=fopen("title.in","r");
     out=fopen("title.out","w");
     fscanf(in,"%s",&s);
-    for(int i=1;i<=s.length();i++)
+    for(int i=0;s[i]!=EOF;i++)
     {
         if((s[i]>='0'&&s[i]<='9')||(s[i]>='A'&&s[i]<='Z')||(s[i]>='a'&&s[i]<='z'))
         {
