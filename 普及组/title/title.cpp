a7d7c9e2ed5d42af682a33f262cc702118ba9fc7
Acetylene <52035894+Acetylene475@users.noreply.github.com>
2019-07-26 17:59:47 +0800

    普及组1.title

---
+++ 普及组/title/title.cpp
@@ -0,0 +1,22 @@
+#include <cstdio>
+#include <string>
+using namespace std;
+
+int main()
+{
+    FILE *in,*out;
+    string s;
+    int count=0;
+    in=fopen("title.in","r");
+    out=fopen("title.out","w");
+    fscanf(in,"%s",&s);
+    for(int i=1;i<=s.length;i++)
+    {
+        if((s[i]>=48&&s[i]<=57)||(s[i]>=65&&s[i]<=90)||(s[i]>=97&&s[i]<=122))
+        {
+            count++;
+        }
+    }
+    fprintf(out,"%d",count);
+    return 0;
+}
